e5e1340e80af0e4ab442fcae221ae5c27e90a1c5
lluddenusgs <pludden@contractor.usgs.gov>
2024-07-05 16:56:19 -0500

    Add the enter id button logic

--- apps/model_eval_viz/config.py
+++ apps/model_eval_viz/config.py
@@ -1,1 +1,7 @@
+import panel as pn
 EX_STATES = ['Alaska', 'Hawaii', 'Puerto Rico', 'Commonwealth of the Northern Mariana Islands','United States Virgin Islands','America Somoa','Guam','District of Columbia','American Samoa']
+entered_points = pn.widgets.TextInput(
+    name='Streamgage Site ID', 
+    placeholder='Streamgage Site ID #',
+    
+    )
