92ac97c8bc00c6f958219d38db232bd3e3607d21
lluddenusgs <pludden@contractor.usgs.gov>
2024-07-17 14:41:48 -0500

    implement date selection

--- apps/model_eval_viz/app.py
+++ apps/model_eval_viz/app.py
@@ -3,6 +3,7 @@ import cartopy.feature as cf
 from cartopy import crs as ccrs
 from config import *
 import dask
+import datetime as dt
 import geopandas as gpd
 import geoviews as gv
 import geoviews.feature as gf
@@ -10,13 +11,16 @@ from geoviews import opts
 import holoviews as hv
 import httpx
 import hvplot.pandas
+import nest_asyncio 
 import numpy as np
 import pandas as pd
 import panel as pn
+from pygeohydro import NWIS
 import re
 import truststore
 import ssl
 
+nest_asyncio.apply()
 # create SSL context for internal intranet and read file
 # this is a method used when calling a library like httpx, urllib3, or requests directly rather than `truststore.inject_into_ssl()`
 # ctx = truststore.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
@@ -27,6 +31,10 @@ import ssl
 # get GeoJSON link
 # states_json = (states_request.json()
 #                .get('simplifiedGeometryGeoJSON'))
+
+
+# nwis class
+nwis = NWIS()
 
 states_path = "./data/geoBoundaries-USA-ADM1_simplified.geojson"
 path = "./data/streamflow_gages_v1_n5390.csv"
@@ -85,6 +93,23 @@ state_list = list(states['shapeName'].unique())
 #sort alphabetically
 state_list.sort()
 
+start_date = pn.widgets.DatePicker(
+    # description = "select start date",
+    name = "select start date",
+    value = dt.date(2001,1,1),
+    start = dt.date(2001,1,1),
+    end = dt.date(2015,12,31),
+
+
+)
+end_date = pn.widgets.DatePicker(
+    # description = "select start date",
+    name = "select end date",
+    value = dt.datetime.now(),
+    start = dt.date(2001,1,1),
+    end = dt.date(2015,12,31),
+
+)
 state_selector = pn.widgets.MultiSelect(
     description="Hold ctrl to toggle multiple states",
     name="Select a state",
@@ -179,6 +204,7 @@ def enter_event(event)->None:
         streamgage_input.placeholder ="Expected Format:[id_no], [id_no2]..."
     else:
         entered_points.value = sg_value
+    
 enter_id = pn.panel(pn.widgets.Button(name='Enter', button_type='primary'))
 
 enter_id.on_click(enter_event)
@@ -207,10 +233,17 @@ def display_points(state_list:list,ids:str, data_set:str)->gv.Points:
     if ids:
         # filter stream_gage to only include the specified IDs
         id_list = [pid.strip() for pid in ids.split(",")]
+
+        site_no = id_list[0]
+        dates = (start_date.value,end_date.value)
+        qobs = nwis.get_streamflow(site_no, dates)
+        print(qobs)
+
         if (id_list != []):
             filt_points = filt_points[filt_points['site_no'].isin(id_list)]
     if data_set != "":
         filt_points = filt_points[(filt_points[data_set]==1).all(axis=1)] 
+    
     selected_points = gv.Points(filt_points).opts(**plot_opts,color='lightgreen', size=5)
 
 
@@ -245,7 +278,7 @@ pn.bind(reset_map, clear_map, watch=True)
 footer = pn.pane.Markdown("""For questions about this application, please visit the [Hytest Repo](https://github.com/hytest-org/hytest/issues)""" ,width=500, height =20)
 
 
-map_modifier = pn.Column(state_selector, map_selector, subset_selector, streamgage_input, enter_id, clear_map,sizing_mode='stretch_width')
+map_modifier = pn.Column(state_selector, map_selector, subset_selector, streamgage_input, enter_id, clear_map, start_date, end_date,sizing_mode='stretch_width')
 
 model_eval = pn.template.FastGridTemplate(
     title="HyTEST Model Evaluation",  
