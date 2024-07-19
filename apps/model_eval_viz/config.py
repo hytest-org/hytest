edeed64d01b9b27749c359fc23857e6e10c9cd95
lluddenusgs <pludden@contractor.usgs.gov>
2024-07-19 15:26:14 -0500

    ruff format

--- apps/model_eval_viz/config.py
+++ apps/model_eval_viz/config.py
@@ -1,6 +1,17 @@
 import panel as pn
 from pygeohydro import NWIS
-#constants and data function calls will be in this file
+
+# constants and data function calls will be in this file
 nwis = NWIS()
-EX_STATES = ['Alaska', 'Hawaii', 'Puerto Rico', 'Commonwealth of the Northern Mariana Islands', 'United States Virgin Islands', 'America Somoa', 'Guam', 'District of Columbia', 'American Samoa']
-STREAMGAGE_SUBSET = ['nldi', 'swim', 'gfv1d1', 'camels']
+EX_STATES = [
+    "Alaska",
+    "Hawaii",
+    "Puerto Rico",
+    "Commonwealth of the Northern Mariana Islands",
+    "United States Virgin Islands",
+    "America Somoa",
+    "Guam",
+    "District of Columbia",
+    "American Samoa",
+]
+STREAMGAGE_SUBSET = ["nldi", "swim", "gfv1d1", "camels"]
