7ab293077221fa3ba55ca7dc9ca01e77996e71f6
lluddenusgs <pludden@contractor.usgs.gov>
2024-07-19 14:13:57 -0500

    config modularity

--- apps/model_eval_viz/config.py
+++ apps/model_eval_viz/config.py
@@ -1,9 +1,6 @@
 import panel as pn
-EX_STATES = ['Alaska', 'Hawaii', 'Puerto Rico', 'Commonwealth of the Northern Mariana Islands','United States Virgin Islands','America Somoa','Guam','District of Columbia','American Samoa']
-entered_points = pn.widgets.TextInput(
-    name='Streamgage Site ID', 
-    placeholder='Streamgage Site ID #',
-    description='Enter a column delimited list e.g. 01022500, 01022502',
-    
-    )
-STREAMGAGE_SUBSET = ['nldi','swim','gfv1d1','camels']
+from pygeohydro import NWIS
+#constants and data function calls will be in this file
+nwis = NWIS()
+EX_STATES = ['Alaska', 'Hawaii', 'Puerto Rico', 'Commonwealth of the Northern Mariana Islands', 'United States Virgin Islands', 'America Somoa', 'Guam', 'District of Columbia', 'American Samoa']
+STREAMGAGE_SUBSET = ['nldi', 'swim', 'gfv1d1', 'camels']
