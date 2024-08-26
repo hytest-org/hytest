cce977f78ee1d2a8c0edccfbdf5246c31fa356ba
Andrew Laws <alaws@usgs.gov>
2024-08-26 11:07:28 -0500

    Updates from ruff

--- apps/model_eval_viz/map.py
+++ apps/model_eval_viz/map.py
@@ -1,8 +1,10 @@
-import param
-from config import STREAMGAGE_SUBSET
+import re
+
 import geoviews as gv
 import panel as pn
-import re
+import param
+
+from config import STREAMGAGE_SUBSET
 
 ### Plot opts  # noqa: E266
 map_plot_opts = dict(
@@ -12,6 +14,7 @@ map_plot_opts = dict(
     xaxis=None,
     yaxis=None
 )
+
 
 class Map(param.Parameterized):
     """Instantiate map of CONUS."""
@@ -58,23 +61,23 @@ class Map(param.Parameterized):
             else:
                 streamgages_to_display = self.streamgages
 
-            if len(self.state_select) > 0: 
-                    streamgages_to_display = (streamgages_to_display.clip(self.states[self.states['shapeName'].isin(self.state_select)]))
+            if len(self.state_select) > 0:
+                streamgages_to_display = (streamgages_to_display.clip(self.states[self.states['shapeName'].isin(self.state_select)]))
 
         return gv.Points(streamgages_to_display).options(
-            cmap="Plasma", # stand in and will be changed later
-            color="complete_yrs", # stand in and will be changed later
-            size=5) # stand in and will be changed later
+            cmap="Plasma",  # stand in and will be changed later
+            color="complete_yrs",  # stand in and will be changed later
+            size=5)  # stand in and will be changed later
 
     @param.depends("display_states", "display_basemap", "display_streamgages")
     def view(self):
         """Merge map components into display."""
         return pn.pane.HoloViews(self.display_basemap() * self.display_states() * self.display_streamgages().options(**map_plot_opts))
-    
+
     # buttons
     @param.depends("search_streamgage_id_input", watch=True)
     def _update_streamgage_input(self):
-        if re.search('[^0-9, ]',self.streamgage_id_input):
+        if re.search('[^0-9, ]', self.streamgage_id_input):
             pn.state.notifications.warning("Search included invalid characters. Please see the tooltip (?) for correct formatting.", duration=5000)
         else:
             self.streamgage_id_string = self.streamgage_id_input
@@ -82,7 +85,7 @@ class Map(param.Parameterized):
     @param.depends("clear_streamgage_id_input", watch=True)
     def _clear_streamgage_input(self):
         self.streamgage_id_string = ""
-        self.streamgage_id_input = ""   
+        self.streamgage_id_input = ""
 
     @param.depends("reset_map", watch=True)
     def _reset_map(self):
